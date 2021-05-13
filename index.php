20b2611ffcbecc0b990438c3a26e0c6dd56b1bcb
Benny <bbxblack@gmail.com>
2021-05-13 16:15:44 +0200

    v0.0

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      $severname = "loacalhost";
+      $username = "root"
+      $password = "root"
+      $dbname = ""
+      // da inserire il nome del database
+
+     ?>
+  </body>
+</html>
