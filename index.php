30c4195ed9c4f01d957dd06baec60f969c8685bd
Benny <bbxblack@gmail.com>
2021-05-16 16:29:27 +0200

    v0.1

--- index.php
+++ index.php
@@ -13,6 +13,13 @@
       // da inserire il nome del database
 
       // Connect 
+      $conn = new mysqli($servername, $username, $password, $dbname);
+
+      if ($conn && $conn->connect_error) {
+        echo "Connection failed" . $conn->connect_error;
+      }else{
+        echo "Connessione riuscita"
+      }
      ?>
   </body>
 </html>
